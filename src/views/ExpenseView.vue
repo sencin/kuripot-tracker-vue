1feffda9ddaaf1678f776227f04d60abc9b0aada
sencin <asunayuk34@gmail.com>
2026-01-04 21:24:47 +0800

    harcoded expense cateogry

--- src/views/ExpenseView.vue
+++ src/views/ExpenseView.vue
@@ -17,13 +17,13 @@
           class="flex flex-col px-4 py-4 gap-2 rounded-xl"
           style="background-image: radial-gradient(circle at left top, var(--p-primary-700), var(--p-primary-900))"
         >
-          <!-- Header Icon -->
+          <!-- Header Icon
           <svg width="30" height="35" viewBox="0 0 35 40" fill="none" class="block mx-auto">
             <path
               d="M25.87 18.05L23.16 17.45L25.27 20.46V29.78L32.49 23.76V13.53L29.18 14.73L25.87 18.04V18.05ZM25.27 35.49L29.18 31.58V27.67L25.27 30.98V35.49ZM20.16 17.14H20.03H20.17H20.16ZM30.1 5.19L34.89 4.81L33.08 12.33L24.1 15.67L30.08 5.2L30.1 5.19ZM5.72 14.74L2.41 13.54V23.77L9.63 29.79V20.47L11.74 17.46L9.03 18.06L5.72 14.75V14.74ZM9.63 30.98L5.72 27.67V31.58L9.63 35.49V30.98ZM4.8 5.2L10.78 15.67L1.81 12.33L0 4.81L4.79 5.19L4.8 5.2ZM24.37 21.05V34.59L22.56 37.29L20.46 39.4H14.44L12.34 37.29L10.53 34.59V21.05L12.42 18.23L17.45 26.8L22.48 18.23L24.37 21.05ZM22.85 0L22.57 0.69L17.45 13.08L12.33 0.69L12.05 0H22.85Z"
               fill="var(--p-primary-200)"
             />
-          </svg>
+          </svg> -->
 
           <!-- Form Fields -->
           <div class="grid gap-2 text-sm">
@@ -114,6 +114,8 @@ const paymentTypeOptions = ref<Option[]>([
 
 const expenseCategoryOptions = ref<Option[]>([
   { id: 1, name: "Bills" },
+  { id: 2, name: "Load" },
+  { id: 3, name: "General Expenses" },
 ]);
 
 const createTransaction = async () => {
