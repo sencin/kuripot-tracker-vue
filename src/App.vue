cae2336e785aaf319af36712d7669bdb0cc890fa
sencin <asunayuk34@gmail.com>
2026-01-04 16:59:39 +0800

    fetch the user on refresh

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import AppNavbar from '@/components/AppNavbar.vue'
+import { useAuthStore } from '@/stores/authenticate'
+import { onMounted } from 'vue'
+
+const authStore = useAuthStore()
+
+onMounted(async () => {
+  await authStore.getUser()
+})
+
 </script>
 
 <template>
@@ -8,8 +17,6 @@ import AppNavbar from '@/components/AppNavbar.vue'
   <header>
       <nav>
         <AppNavbar />
-<!--        <RouterLink to="/">Home</RouterLink>-->
-<!--        <RouterLink to="/about">About</RouterLink>-->
       </nav>
   </header>
   <RouterView />
