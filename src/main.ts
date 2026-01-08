124b580550c1a85965f3951c3e43e9d568315139
sencin <asunayuk34@gmail.com>
2026-01-08 20:16:19 +0800

    removed the toast here

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import 'primeicons/primeicons.css'
 import App from './App.vue'
 import router from './router'
 import ToastService from 'primevue/toastservice';
-import Toast from 'primevue/toast';
+
 const app = createApp(App)
-app.component('Toast', Toast);
+
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
